cf5829e84936df22bca7cee2aaa0089fbd86a7b8
xibei <15281741661@qq.com>
2025-05-10 22:12:57 +0800

    proj1: 完成array和LinkedListDeque的基本功能

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -10,40 +10,64 @@ import java.util.Objects;
 public class ArrayDeque<T> implements Deque<T> {
     private T[] items;
     private int size;
+    private int nextFirst;
+    private int nextLast;
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    public boolean isFull() {
+        return this.size == items.length;
     }
 
     public void resize(int capacity) {
-        T[] alter = (T[]) new Object[capacity];
-        System.arraycopy(items, 0, alter, 0, size);
-        items = alter;
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
     }
 
     @Override
     public void addFirst(T item) {
-        items[size-1] = item;
+        // 判断是否为满，满则resize
+//        if (isFull()) {
+//            resize(16);
+//        }
+        // 赋值
+        items[nextFirst] = item;
+        // nextFirst指针移动
+        nextFirst = nextFirst == 0 ? items.length-1 : nextFirst-1;
         size++;
     }
 
     @Override
     public void addLast(T item) {
-        items[size] = item;
+        // 判断是否为满，满则resize
+//        if (isFull()) {
+//            resize(16);
+//        }
+        // 赋值
+        items[nextLast] = item;
+        // nextLast指针移动
+        nextLast = nextLast == items.length-1 ? 0 : nextLast+1;
         size++;
     }
 
     @Override
     public T removeFirst() {
         size--;
-        return items[0];
+        nextFirst++;
+        return items[nextFirst-1];
     }
 
     @Override
     public T removeLast() {
         size--;
-        return items[size+1];
+        nextLast--;
+        return items[nextLast+1];
     }
 
     @Override
@@ -58,7 +82,7 @@ public class ArrayDeque<T> implements Deque<T> {
 
     @Override
     public void printDeque() {
-        for(int i = 0; i < size; i++) {
+        for(int i = nextFirst+1; i <= nextLast-1; i++) {
             System.out.print(items[i] + " ");
         }
     }
